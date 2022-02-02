3fcaf48e68b10f4ca000d574ea7defcd6e27a7c3
Hannah Malm <hannahemalm616@gmail.com>
2022-02-01 18:27:07 -0600

    commit tree 1ffff38f7c927f2f1610e0ec1c0999dcf96ef725

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+// . Use the event's callback function to target the paragraph element with id="text" 
+// and replace the text with "This is really cool!"
+
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log("The DOM has loaded");
+    const p = document.getElementById('text')
+    p.innerHTML = "This is really cool!"
+
+  });
